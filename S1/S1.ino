174afe2a92404a9445a8dd75d1304545adaed2ec
JoaoVieiraa07 <joao-vitor_vieira@estudante.sesisenai.org.br>
2025-11-24 22:42:10 -0300

    adicionando funções de controle do LED RGB

--- S1/S1.ino
+++ S1/S1.ino
@@ -44,3 +44,46 @@ const char* WIFI_PASS = ENV_PASS;
 const char* BROKER = BROKER_URL;
 const int BROKER_PORT = BROKER_PORT;
 const char* TOPIC = TOPIC1;
+
+
+//FUNÇÃO PARA CONTROLAR CORES
+void setLED(byte r, byte g, byte b) {
+  ledcWrite(CH_R, r);
+  ledcWrite(CH_G, g);
+  ledcWrite(CH_B, b);
+}
+
+
+//LED RGB INDICANDO ESTADOS
+void statusLED(byte estado) {
+
+  setLED(0, 0, 0); // Sempre apaga antes
+
+  switch (estado) {
+
+    case 1: // Amarelo → conectando WiFi
+      setLED(255, 255, 0);
+      break;
+
+    case 2: // Roxo → conectando ao Broker
+      setLED(180, 0, 255);
+      break;
+
+    case 3: // Verde → tudo OK
+      setLED(0, 255, 0);
+      break;
+
+    case 254: // Vermelho → erro
+      setLED(255, 0, 0);
+      break;
+
+    default: // Pisca azul → recebeu mensagem
+      for (int i = 0; i < 4; i++) {
+        setLED(0, 0, 255);
+        delay(100);
+        setLED(0, 0, 0);
+        delay(100);
+      }
+      break;
+  }
+}
