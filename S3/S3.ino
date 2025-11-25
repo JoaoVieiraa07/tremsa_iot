9e10016d669ddbb3b60498e90098d452b19cde85
DiogoCunhaaa <diogo_f_cunha@estudante.sesisenai.org.br>
2025-11-24 22:23:10 -0300

    Reza pai

--- S3/S3.ino
+++ S3/S3.ino
@@ -2,14 +2,21 @@
 #include <PubSubClient.h>
 #include "env.h"
 #include <WiFiClientSecure.h>
-
-// --- WiFi & MQTT Configuration ---
-const char* WIFI_SSID = ENV_SSID;
-const char* WIFI_PASS = ENV_PASS;
+#include <Servo.h>
 
 const char* brokerURL = BROKER_URL;
 const int brokerPort = BROKER_PORT;
-const char* mqttTopic = TOPIC3;  // Tópico usado para publish/subscribe
+
+const int ULTRA_ECHO3 = 18;
+const int ULTRA_TRIG3 = 19;
+
+Servo SERVO_1;
+Servo SERVO_2;
+
+// ENV_TOPIC_PRESENCA subscribe
+// ENV_TOPIC_PRESENCA publish
+// ENV_TOPIC_SERVO1 publish
+// ENV_TOPIC_SERVO2 publish
 
 WiFiClientSecure client;
 PubSubClient mqtt(client);
@@ -26,24 +33,44 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Mensagem recebida via MQTT: ");
   Serial.println(msgRecebida);
 
-  // Verifica se é um comando para o LED
+  // Verifica se é um comando para os servos
   if (msgRecebida == "1") {
-    digitalWrite(ledPin, HIGH);
-    Serial.println("LED ligado via MQTT");
-    mqtt.publish(mqttTopic, "LED ligado via MQTT");
-  } else if (msgRecebida == "0") {
-    digitalWrite(ledPin, LOW);
-    Serial.println("LED desligado via MQTT");
-    mqtt.publish(mqttTopic, "LED desligado via MQTT");
-  }
+      SERVO_1.write(180);
+      delay(1000);
+      SERVO_1.write(0);
+    } else if (msgRecebida == "2") {
+      SERVO_2.write(180);
+      delay(1000);
+      SERVO_2.write(0);
+    } else {
+      Serial.printf("Comando invalido");
+    }
+}
+
+long lerDistancia() {
+  digitalWrite(ULTRA_TRIG3, LOW);
+  delayMicroseconds(2);
+  digitalWrite(ULTRA_TRIG3, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(ULTRA_TRIG3, LOW);
+  
+  long duracao = pulseIn(ULTRA_ECHO3, HIGH);
+  long distancia = duracao * 349.24 / 2 / 10000;
+  
+  return distancia;
 }
 
 void setup() {
   Serial.begin(115200);
   client.setInsecure();
   
-  // Configura o LED como saída
+  // Configura as entradas/saidas
   pinMode(ledPin, OUTPUT);
+  pinMode(ULTRA_TRIG3, OUTPUT);
+  pinMode(ULTRA_ECHO3, INPUT);
+
+  SERVO_1.attach(22);
+  SERVO_2.attach(23);
 
   // Conexão Wi-Fi
   WiFi.begin(ENV_SSID, ENV_PASS);
@@ -59,35 +86,45 @@ void setup() {
   mqtt.setCallback(callback);
 
   Serial.println("Conectando ao broker MQTT...");
-  String boardID = "S1-" + String(random(0xffff), HEX);
+  String boardID = "S3-" + String(random(0xffff), HEX);
 
   while (!mqtt.connect(boardID.c_str(), BROKER_USR_NAME, BROKER_USR_PASS)) {
     Serial.print(".");
     delay(200);
   }
 
-  mqtt.subscribe(mqttTopic);  // Inscreve-se no tópico
+  mqtt.subscribe(ENV_TOPIC_PRESENCA);  // Inscreve-se no tópico
   Serial.println("\nConectado ao broker MQTT e inscrito no tópico");
 }
 
 void loop() {
   mqtt.loop();  // Mantém a conexão MQTT ativa
 
+  long distancia = lerDistancia();
+
+  Serial.printf("Distância: %ld cm.\n", distancia);
+
+  if (distancia > 0 && distancia < 5) {
+    mqtt.publish(ENV_TOPIC_PRESENCA, "1");
+    Serial.println("Trem detectado!");
+    delay(300);
+  }
+
   // Lê comandos pela serial
   if (Serial.available()) {
-    String comando = Serial.readStringUntil('\n');
-    comando.trim();  // Remove espaços em branco
-
-    if (comando == "1") {
-      digitalWrite(ledPin, HIGH);
-      Serial.println("LED ligado via Serial");
-      mqtt.publish(mqttTopic, "LED ligado via Serial");
-    } else if (comando == "0") {
-      digitalWrite(ledPin, LOW);
-      Serial.println("LED desligado via Serial");
-      mqtt.publish(mqttTopic, "LED desligado via Serial");
+    String cmd = Serial.readStringUntil('\n');
+    cmd.trim();  // Remove espaços em branco
+    
+    if (cmd == "1") {
+      SERVO_1.write(180);
+      delay(1000);
+      SERVO_1.write(0);
+    } else if (cmd == "2") {
+      SERVO_2.write(180);
+      delay(1000);
+      SERVO_2.write(0);
     } else {
-      Serial.println("Comando inválido. Use 1 (ligar) ou 0 (desligar).");
+      Serial.printf("Comando invalido");
     }
   }
 }
