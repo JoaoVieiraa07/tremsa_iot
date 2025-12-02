8a432d941c5db7e367b6f20037fea0de90999d40
MatheusPicollik <oliveira.matheuspicolli@gmail.com>
2025-12-02 11:04:27 -0300

    espero que sim 2

--- S2/S2.ino
+++ S2/S2.ino
@@ -1,41 +1,59 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
-#include <Servo.h>
 #include "wifi_connection.h"
 
-// WIFI e MQTT
+// ====== WIFI E MQTT ======
 const char* ssid     = "";
 const char* password = "";
-
 const char* mqtt_server = "";
+
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-// PINOS
-#define PIN_PRESENCA1  32
-#define PIN_PRESENCA2  33
-#define PIN_SERVO      14
-#define PIN_LED_R      25
-#define PIN_LED_G      26
-#define PIN_LED_B      27
+// Ultrassônico 1
+#define TRIG1 32
+#define ECHO1 33
+
+// Ultrassônico 2
+#define TRIG2 25
+#define ECHO2 26
+
+// LED RGB
+#define LED_R 14
+#define LED_G 27
+#define LED_B 12
+
+// led normal
+#define LED_SIMPLE 13
 
 
+// leitura sensor ultrassonico
+long readUltrasonic(int trig, int echo) {
+  digitalWrite(trig, LOW);
+  delayMicroseconds(4);
 
-Servo servoS2;
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
 
-int presenca1_ant = HIGH;
-int presenca2_ant = HIGH;
+  long duration = pulseIn(echo, HIGH, 30000);  
+  long distance = duration * 0.034 / 2; // em cm
+
+  if (distance == 0) distance = -1; // erro
+
+  return distance;
+}
 
 
+// mqtt roconnect
 void reconnect() {
   while (!client.connected()) {
     Serial.print("Conectando ao MQTT...");
     if (client.connect("S2")) {
       Serial.println(" conectado!");
 
-      // tópicos que a S2 ESCUTA
-      client.subscribe("S2/Servo1");
-      client.subscribe("S2/LED");
+      client.subscribe("S2/LED_RGB");
+      client.subscribe("S2/LED_SIMPLE");
 
     } else {
       Serial.print("falhou, rc=");
@@ -47,26 +65,15 @@ void reconnect() {
 }
 
 
+// ====== CALLBACK MQTT ======
 void callback(char* topic, byte* message, unsigned int length) {
   String msg = "";
   for (int i = 0; i < length; i++) msg += (char)message[i];
 
-  Serial.print("Recebido [");
-  Serial.print(topic);
-  Serial.print("]: ");
-  Serial.println(msg);
+  Serial.printf("Mensagem [%s]: %s\n", topic, msg.c_str());
 
-  // SERVO
-  if (String(topic) == "S2/Servo1") {
-    int pos = msg.toInt();
-    if (pos >= 0 && pos <= 180) {
-      servoS2.write(pos);
-    }
-  }
-
-  // LED
-  if (String(topic) == "S2/LED") {
-    // mensagem no formato: R,G,B (ex: 255,0,100)
+  // LED RGB (formato: R,G,B)
+  if (String(topic) == "S2/LED_RGB") {
     int p1 = msg.indexOf(',');
     int p2 = msg.indexOf(',', p1 + 1);
 
@@ -74,27 +81,33 @@ void callback(char* topic, byte* message, unsigned int length) {
     int g = msg.substring(p1 + 1, p2).toInt();
     int b = msg.substring(p2 + 1).toInt();
 
-    analogWrite(PIN_LED_R, r);
-    analogWrite(PIN_LED_G, g);
-    analogWrite(PIN_LED_B, b);
+    analogWrite(LED_R, r);
+    analogWrite(LED_G, g);
+    analogWrite(LED_B, b);
+  }
+
+  // LED simples (0 ou 1)
+  if (String(topic) == "S2/LED_SIMPLE") {
+    digitalWrite(LED_SIMPLE, msg == "1" ? HIGH : LOW);
   }
 }
 
+
+// SETUP
 void setup() {
   Serial.begin(115200);
 
-  
-  pinMode(PIN_PRESENCA1, INPUT_PULLUP);
-  pinMode(PIN_PRESENCA2, INPUT_PULLUP);
+  // Ultrassônicos
+  pinMode(TRIG1, OUTPUT);
+  pinMode(ECHO1, INPUT);
+  pinMode(TRIG2, OUTPUT);
+  pinMode(ECHO2, INPUT);
 
-  
-  pinMode(PIN_LED_R, OUTPUT);
-  pinMode(PIN_LED_G, OUTPUT);
-  pinMode(PIN_LED_B, OUTPUT);
-
- 
-  servoS2.attach(PIN_SERVO);
-  servoS2.write(90); // inicial
+  // LEDs
+  pinMode(LED_R, OUTPUT);
+  pinMode(LED_G, OUTPUT);
+  pinMode(LED_B, OUTPUT);
+  pinMode(LED_SIMPLE, OUTPUT);
 
   // WiFi
   wifi_connect(ssid, password);
@@ -104,22 +117,24 @@ void setup() {
   client.setCallback(callback);
 }
 
+
+// LOOP
 void loop() {
   if (!client.connected()) reconnect();
   client.loop();
 
-  int p1 = digitalRead(PIN_PRESENCA1);
-  int p2 = digitalRead(PIN_PRESENCA2);
+  // Leitura dos sensores
+  long dist1 = readUltrasonic(TRIG1, ECHO1);
+  long dist2 = readUltrasonic(TRIG2, ECHO2);
 
-  if (p1 != presenca1_ant) {
-    presenca1_ant = p1;
-    client.publish("S2/Presenca1", p1 == LOW ? "1" : "0");
-  }
+  // Publica no MQTT
+  char buffer[10];
 
-  if (p2 != presenca2_ant) {
-    presenca2_ant = p2;
-    client.publish("S2/Presenca2", p2 == LOW ? "1" : "0");
-  }
+  sprintf(buffer, "%ld", dist1);
+  client.publish("S2/Distancia1", buffer);
 
-  delay(50);
+  sprintf(buffer, "%ld", dist2);
+  client.publish("S2/Distancia2", buffer);
+
+  delay(300);
 }
