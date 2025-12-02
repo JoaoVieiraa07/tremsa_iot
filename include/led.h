95586b557fa0a62edb6f69a85a3a702c34e842ff
Diogo Cunha <diogo_f_cunha@estudante.sesisenai.org.br>
2025-12-02 10:48:40 -0300

    Delete include directory

--- include/led.h
+++
@@ -1,7 +0,0 @@
-#ifndef WIFI_H
-#define WIFI_H
-
-void setLED(byte r, byte g, byte b);
-void statusLED(byte estado);
-
-#endif // WIFI_H
--- include/wifi_connection.h
+++
@@ -1,6 +0,0 @@
-#ifndef WIFI_INCLUDE_H
-#define WIFI_INCLUDE_H
-
-void wifi_connect(const char* SSID, const char* PASS);
-
-#endif // WIFI_INCLUDE_H
